de659123be497d22dbccfce50318e620367d2866
fx <fx@fx.local>
2019-04-10 19:53:29 +0800

    d

---
+++ d/TO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testignore
+//
+//  Created by fx on 2019/4/10.
+//  Copyright © 2019 fx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
