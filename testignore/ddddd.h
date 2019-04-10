d1a74be8aec436e59f58fbfa28870d22f3e677d1
fx <fx@fx.local>
2019-04-10 18:18:08 +0800

    d

--- testignore/ddddd.h
+++ testignore/ddddd.h
@@ -11,7 +11,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ddddd : NSObject
-
+dddd
 @end
 
 NS_ASSUME_NONNULL_END
---
+++ testignore/dddddddddddd.m
@@ -0,0 +1,13 @@
+//
+//  dddddddddddd.m
+//  testignore
+//
+//  Created by fx on 2019/4/10.
+//  Copyright © 2019 fx. All rights reserved.
+//
+
+#import "dddddddddddd.h"
+
+@implementation dddddddddddd
+
+@end
