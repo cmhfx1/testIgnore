a415d234e0cc0014d73255fbabc6d3fdce50fdfb
fx <fx@fx.local>
2019-04-10 14:38:26 +0800

    初始化工程

---
+++ testignore/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  testignore
+//
+//  Created by fx on 2019/4/10.
+//  Copyright © 2019 fx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ testignore/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testignore
+//
+//  Created by fx on 2019/4/10.
+//  Copyright © 2019 fx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
